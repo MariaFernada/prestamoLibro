e1ce9682f5d0d190d512f58a0c5fcb42b2017c9f
laura <lafecaco@gmail.com>
2019-11-16 20:31:59 -0500

    cambio a bootstrap 4

--- prestamos/webservice-client.php
+++ prestamos/webservice-client.php
@@ -101,9 +101,10 @@
   <title>Book Store Web Service</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+		integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+		integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 </head>
 <body>
 
@@ -114,7 +115,7 @@
   <hr>
   <div class='row'>
   <form class="form-inline" method = 'post' name='form1'>
-        <button type="submit" name='sub_all' class="btn btn-default">Información de todos los libros</button>
+        <button type="submit" name='sub_all' class="btn btn-dark">Información de todos los libros</button>
 
     </form>
      <br /> 
